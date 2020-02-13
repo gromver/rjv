6e8d1a50c86f8f910540640da95bff58e4348e06
Roman Gayazov <gromver5@gmail.com>
2020-02-13 09:17:34 +0300

    fix: index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,14 @@
 import * as events from './events';
 import * as types from './types';
-// import { ISchema, IKeyword, IRuleValidationResult, IRule } from './types';
-// import { StateTypes } from './interfaces/IState';
 
 const version = '${RJV-VERSION}';
 
-export { default as Model } from './ModelOld';
+export { default as Model } from './Model';
 
 export { default as Ref } from './Ref';
-
-// export { default as ISchema } from './interfaces/ISchema';
-//
-// export { default as IKeyword } from './interfaces/IKeyword';
-//
-// export { default as IRuleValidationResult } from './interfaces/IRuleValidationResult';
-//
-// export { default as IRule } from './interfaces/IRule';
 
 export {
   version,
   events,
-  // stateTypes,
   types,
 };
